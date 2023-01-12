d09ca099fafc78a894c570f7ae75e487c8cdb591
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-12 08:08:01 +0530

    added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a=100;
+        int b=200;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
