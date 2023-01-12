dee0d6bbc206a8c918f604657ae12bfa81d6e013
Sukhdeep1519 <121181519+Sukhdeep1519@users.noreply.github.com>
2023-01-11 23:49:14 -0500

    Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a=100;
-        int b=200;
-        int c=300;
+        int b
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println("Version control");
         System.out.println("Harry's changes");
-        System.out.println("Committed by Bharat");
+        System.out.println("sk");
 
     }
 }
