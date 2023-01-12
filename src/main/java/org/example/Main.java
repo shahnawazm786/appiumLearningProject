6fcd0230f320972c9bcbbe4bcaba40ce9f7b5166
61414 <sidhuraman653@yahoo.com>
2023-01-11 22:45:45 -0500

    ramantest2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println(c);
         System.out.println(1);
         System.out.println(2);
-        System.out.println(3);
+        System.out.println(3.);
 
 
     }
