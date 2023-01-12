b7b9d9233d1d3881327bab917b9d7f27d53a52ab
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-12 08:09:59 +0530

    appium learning 12 Jan 2023

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,10 @@ public class Main {
         System.out.println("Hello world!");
         int a=100;
         int b=200;
+        int c=300;
         System.out.println(a);
         System.out.println(b);
+        System.out.println(c);
+
     }
 }
