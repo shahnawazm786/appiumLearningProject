90b3d998440d4e03e0c6e30ae6bca82e1511bc5b
61414 <sidhuraman653@yahoo.com>
2023-01-12 21:04:13 -0500

    Raman - 13 Jan 2023

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a=100;
-        int b=200;
-        int c=300;
+        int b=100,c=200;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        System.out.println("I have added one line code");
+        System.out.println("Version control");
+        System.out.println("Harry's changes");
+        System.out.println("sk");
 
     }
 }
