77c27b249d8c8abee0aeaa12638ea24943c56ec0
Sukhdeep1519 <121181519+Sukhdeep1519@users.noreply.github.com>
2023-01-12 21:36:57 -0500

    well

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a=100;
-        int b=200;
-        int c=300;
+        int b=100,c=200;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        System.out.println("I have added one line code");
-        System.out.println("I have added one line code");
-        System.out.println("Commit Bharat");
-        System.out.println("Commit by Bharat");
-    System.out.println("Commit by sukhdeep");
-}
+        System.out.println("Version control");
+        System.out.println("Harry's changes");
+        System.out.println("well work");
 
-
+    }
 }
