a95aa193702c767faf1db13c9cba0da61ae1e21b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-13 07:37:07 +0530

    shahnawaz - 13 Jan 2023

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int a=100;
-        int b=100,c=200;
+        int b=200;
+        int c=300;
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        System.out.println("Version control");
-        System.out.println("Harry's changes");
-        System.out.println("sk");
+        System.out.println("I have added one line code");
 
     }
 }
