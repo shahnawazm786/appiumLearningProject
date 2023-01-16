5eeda945825c25308c21d4431311bb275339bb28
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-16 06:58:29 +0530

    shahnawaz - 15 Jan 2023 Calculator Test

--- src/main/java/mobile/CalculatorTest.java
+++ src/main/java/mobile/CalculatorTest.java
@@ -1,8 +1,12 @@
 package mobile;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,19 +14,18 @@ import java.net.URL;
 public class CalculatorTest {
 
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws MalformedURLException {
+        openCalculatorDevice();
     }
     public static void openCalculatorDevice() throws MalformedURLException {
         DesiredCapabilities capabilities=new DesiredCapabilities();
         capabilities.setCapability("deviceName","vivo 1920");
         capabilities.setCapability("udid","26976198");
         capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","12");
-
+        capabilities.setCapability("platformVersion","12");
         capabilities.setCapability("appPackage","com.android.bbkcalculator");
         capabilities.setCapability("appActivity","com.android.bbkcalculator.Calculator");
-        AppiumDriver<WebElement> driver = new AppiumDriver<WebElement>(new URL("http://target_ip:used_port/wd/hub"),capabilities);
-
+        AppiumDriver<MobileElement> driver=new AppiumDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        //WebDriver driver=new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
     }
 }
