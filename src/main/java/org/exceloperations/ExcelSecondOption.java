a2dda1f5d26ff8753eb04df19b48af75572ed077
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-18 09:41:36 +0530

    shahnawaz - 18 Jan 2023 Calculator Test

---
+++ src/main/java/org/exceloperations/ExcelSecondOption.java
@@ -0,0 +1,36 @@
+package org.exceloperations;
+
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class ExcelSecondOption {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        File file=new File("C:\\Users\\shahn\\OneDrive\\Desktop\\Appium-Batch-Morning\\TestCases.xlsx");
+        FileInputStream fileInputStream=new FileInputStream(file);
+        if(fileInputStream==null){
+            throw new FileNotFoundException("File not found....");
+        }
+        else {
+
+            HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
+            HSSFSheet sheet = workbook.getSheetAt(0);
+            for(Row row: sheet){
+                for(Cell cell:row){
+                    System.out.print(cell.getStringCellValue()+"\t");
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
