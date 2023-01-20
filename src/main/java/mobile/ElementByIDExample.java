c60aee7ee390acbd7980250913e593b1a26ae5f0
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-20 08:54:34 +0530

    shahnawaz - 20 Jan 2023 API-Demos demo

--- src/main/java/mobile/ElementByIDExample.java
+++ src/main/java/mobile/ElementByIDExample.java
@@ -1,7 +1,33 @@
 package mobile;
 
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.List;
+
 public class ElementByIDExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+
+        capabilities.setCapability("deviceName","vivo 1920");
+        capabilities.setCapability("udid","26976198");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("platformVersion","12");
+        capabilities.setCapability("appium:appPackage","io.appium.android.apis");
+        capabilities.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+        capabilities.setCapability("noReset","true");
+        AppiumDriver driver=new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub/"),capabilities);
+        Thread.sleep(10000);
+        driver.findElement(AppiumBy.accessibilityId("Accessibility")).click();
+        Thread.sleep(20000);
+        List<WebElement> elements = driver.findElements(AppiumBy.id("android:id/text1"));
+        for(WebElement ele:elements){
+            ele.click();
+        }
 
     }
 }
