62ddd6da53a268a396586daf0de474cf84aed86f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-20 09:23:33 +0530

    shahnawaz - 20 Jan 2023 API-Demos demo

---
+++ src/main/java/mobile/ElementByClassNameExample.java
@@ -0,0 +1,23 @@
+package mobile;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class ElementByClassNameExample {
+    public static void main(String[] args) throws  Exception{
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+
+        capabilities.setCapability("deviceName","vivo 1920");
+        capabilities.setCapability("udid","26976198");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("platformVersion","12");
+        capabilities.setCapability("appium:appPackage","io.appium.android.apis");
+        capabilities.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+        capabilities.setCapability("noReset","true");
+        AppiumDriver driver=new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub/"),capabilities);
+        Thread.sleep(1000);
+        
+    }
+}
