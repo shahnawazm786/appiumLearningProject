f37f589ec3e81c7df6e71b427f261e83695d2f1d
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-21 15:48:31 +0530

    shahnawaz - 21 Jan 2023 API-Demos demo

--- src/main/java/org/extentreportsdemo/ExtenReportExample.java
+++ src/main/java/org/extentreportsdemo/ExtenReportExample.java
@@ -2,6 +2,7 @@ package org.extentreportsdemo;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -21,4 +22,22 @@ ExtentReports reports;
     System.out.println("First Test Case001");
     System.out.println("First Test Case001");
 }
+    @Test
+    public void TestCase002(){
+        System.out.println("Second Test Case001");
+        System.out.println("Second Test Case001");
+        System.out.println("Second Test Case001");
+    }
+    @Test
+    public void TestCase003(){
+        System.out.println("Third Test Case001");
+        System.out.println("Third Test Case001");
+        System.out.println("Third Test Case001");
+    }
+
+    @AfterSuite
+    public void tearDown(){
+    reports.flush();
+    }
+
 }
