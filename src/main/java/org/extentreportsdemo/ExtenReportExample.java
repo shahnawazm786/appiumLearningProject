8b4ec1ff6d0dbd7d9c27a6adca916415194134c5
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-21 15:46:38 +0530

    shahnawaz - 21 Jan 2023 API-Demos demo

--- src/main/java/org/extentreportsdemo/ExtenReportExample.java
+++ src/main/java/org/extentreportsdemo/ExtenReportExample.java
@@ -1,5 +1,24 @@
 package org.extentreportsdemo;
 
-public class ExtenReportExample {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+public class ExtenReportExample {
+ExtentSparkReporter htmlReporter;
+ExtentReports reports;
+@BeforeSuite
+    public void setup(){
+    htmlReporter=new ExtentSparkReporter("extent-report.html");
+    reports=new ExtentReports();
+    reports.attachReporter(htmlReporter);
+}
+@Test
+    public void TestCase001(){
+    System.out.println("First Test Case001");
+    System.out.println("First Test Case001");
+    System.out.println("First Test Case001");
+}
 }
