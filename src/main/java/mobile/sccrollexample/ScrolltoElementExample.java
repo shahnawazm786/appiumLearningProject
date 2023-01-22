c81abe3d0f911cac81b9e977193345f80af4562f
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-22 17:43:07 +0530

    shahnawaz - 22 Jan 2023 API-Demos demo

---
+++ src/main/java/mobile/sccrollexample/ScrolltoElementExample.java
@@ -0,0 +1,34 @@
+package mobile.sccrollexample;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+public class ScrolltoElementExample {
+    @Test
+    public void scrollToElement(){
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+
+        capabilities.setCapability("deviceName","vivo 1920");
+        capabilities.setCapability("udid","26976198");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("platformVersion","12");
+        capabilities.setCapability("appium:appPackage","io.appium.android.apis");
+        capabilities.setCapability("appActivity","io.appium.android.apis.ApiDemos");
+        capabilities.setCapability("noReset","true");
+        AppiumDriver driver=new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub/"),capabilities);
+        Thread.sleep(1000);
+
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Views\"]")).click();
+        Thread.sleep(1000);
+
+        Thread.sleep(1000);
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Filter\"]")).click();
+        // search
+        driver.findElement(AppiumBy.id("android:id/search_src_text")).sendKeys("Appium Class");
+        Thread.sleep(5000);
+    }
+}
