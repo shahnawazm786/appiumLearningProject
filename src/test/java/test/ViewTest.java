378b23bfde28505ce099320d500fd45a7d5fe616
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-24 08:04:58 +0530

    shahnawaz - 24 Jan 2023 API-Demos demo

--- src/test/java/test/ViewTest.java
+++ src/test/java/test/ViewTest.java
@@ -4,15 +4,12 @@ package test;
 import org.testng.annotations.Test;
 import page.ViewPage;
 
-
-
-
 public class ViewTest extends BaseClassTest{
 
      ViewPage viewPage;//=new ViewPage(getDriver());
     @Test
     public void clickAccessibility(){
-        viewPage=new ViewPage(getDriver());
+        //viewPage=new ViewPage(getDriver());
         viewPage.tapAccessibilityElement();
     }
 
